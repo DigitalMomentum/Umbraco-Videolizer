942f2b25c27e2e38c0603d46f7ab793e9a63ac97
David Sheiles <david@digitalmomentum.com.au>
2016-09-21 22:24:51 +1000

    Add Package Migrations and Update to create DataType on AppStart

---
+++ Videolizer/Migrations/MigrationEvents.cs
@@ -0,0 +1,49 @@
+﻿using Semver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Persistence.Migrations;
+using Umbraco.Web;
+
+namespace Videolizer.Migrations {
+    public class MigrationEvents : ApplicationEventHandler {
+
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext) {
+            HandleMigration();
+        }
+
+        private void HandleMigration() {
+            const string productName = "Videolizer";
+            var currentVersion = new SemVersion(0, 0, 0);
+
+            // get all migrations for "Statistics" already executed
+            var migrations = ApplicationContext.Current.Services.MigrationEntryService.GetAll(productName);
+
+            // get the latest migration for "Statistics" executed
+            var latestMigration = migrations.OrderByDescending(x => x.Version).FirstOrDefault();
+
+            if (latestMigration != null)
+                currentVersion = latestMigration.Version;
+
+            var targetVersion = new SemVersion(1, 0, 0);
+            if (targetVersion == currentVersion)
+                return;
+
+            var migrationsRunner = new MigrationRunner(
+              ApplicationContext.Current.Services.MigrationEntryService,
+              ApplicationContext.Current.ProfilingLogger.Logger,
+              currentVersion,
+              targetVersion,
+              productName);
+
+            try {
+                migrationsRunner.Execute(UmbracoContext.Current.Application.DatabaseContext.Database);
+            } catch (Exception e) {
+                LogHelper.Error<MigrationEvents>("Error running Statistics migration", e);
+            }
+        }
+    }
+}
---
+++ Videolizer/Migrations/Version_1_1_0.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Models;
+using Umbraco.Core.Persistence.Migrations;
+using Umbraco.Core.Persistence.SqlSyntax;
+using Umbraco.Core.Services;
+
+namespace Videolizer.Migrations {
+    [Migration("1.0.0", 1, "Videolizer")]
+    public class Version_1_1_0 : MigrationBase {
+        public Version_1_1_0(ISqlSyntaxProvider sqlSyntax, ILogger logger) : base(sqlSyntax, logger) {
+        }
+
+        public override void Up() {
+            //Create a new Data Type called "Videolizer"
+            DataTypeService dataTypeService = (DataTypeService)ApplicationContext.Current.Services.DataTypeService;
+
+            if (dataTypeService.GetDataTypeDefinitionByName("Videolizer") == null) { //check to see if it already exists
+                //Create a new one
+                DataTypeDefinition VideolizerDataTypeDef = new DataTypeDefinition("DigitalMomentum.Videolizer");
+                VideolizerDataTypeDef.Name = "Videolizer";
+
+                dataTypeService.Save(VideolizerDataTypeDef);
+            }
+
+
+
+
+        }
+
+        public override void Down() {
+            //Nothing to do!
+        }
+    }
+}
